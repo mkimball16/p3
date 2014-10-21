20a65f95489e0a06c2970fc427d4daa96f4822be
MacKenzie Kimball <mackenziekimball@gmail.com>
2014-10-21 16:13:44 -0400

    Fixing typo on user page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -18,7 +18,7 @@ a magic lantern? Well, your wish is my comand. The links above will take you to 
 you can wish for a specific number of random users or paragraphs of lorem ipsum text you would 
 like to receive. Just click one of the links and indicate the desired number of users or paragraphs by 
 entering a number between 1-99 to the form. For the random user geneteror, you can also indicate if you 
-to inclue birthday, profile text, email, and phone number with each user in your results. Then just
+would like to inclue birthday, profile text, email, and phone number with each user in your results. Then just
 press the Submit button and your wish will be granted!</p>
 
 	
